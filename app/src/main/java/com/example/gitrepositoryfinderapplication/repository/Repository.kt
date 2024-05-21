28363efa4cbf71141613b6b37767c07cd71f6482
Aliaksandr <dyvedd@gmail.com>
2024-05-21 12:21:35 +0300

    create view model

---
+++ app/src/main/java/com/example/gitrepositoryfinderapplication/repository/Repository.kt
@@ -0,0 +1,8 @@
+package com.example.gitrepositoryfinderapplication.repository
+
+import com.example.gitrepositoryfinderapplication.db.GitRepositoryDatabase
+
+class Repository(
+	val db: GitRepositoryDatabase
+) {
+}
--- app/src/main/java/com/example/gitrepositoryfinderapplication/ui/fragments/SearchGitRepositoriesFragment.kt
+++ app/src/main/java/com/example/gitrepositoryfinderapplication/ui/fragments/SearchGitRepositoriesFragment.kt
@@ -1,7 +1,18 @@
 package com.example.gitrepositoryfinderapplication.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.gitrepositoryfinderapplication.R
+import com.example.gitrepositoryfinderapplication.ui.MainActivity
+import com.example.gitrepositoryfinderapplication.ui.viewmodel.GitRepositoryViewModel
 
-class SearchGitRepositoriesFragment:Fragment(R.layout.fragment_search_git_repositories) {
+class SearchGitRepositoriesFragment : Fragment(R.layout.fragment_search_git_repositories) {
+
+	lateinit var viewModel: GitRepositoryViewModel
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		viewModel = (activity as MainActivity).viewModel
+	}
 }
